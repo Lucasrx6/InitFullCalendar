e841d2294a29b03623d237a5e9a0b91bff52ce75
lucasrx6 <lucasrx6@gmail.com>
2023-11-24 11:15:33 -0300

    Atualizando o calendario

--- scripts.js
+++ scripts.js
@@ -1,8 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarioEl = document.getElementById('calendario');
-    var calendario = new FullCalendar.Calendar(calendarioEl, {
-
+  var calendarioEl = document.getElementById('calendario');
+  var calendario = new FullCalendar.Calendar(calendarioEl, {
       locale: 'pt-br'
-    });
-    calendario.render();
   });
+  calendario.render();
+
+  // Capturando o formulário
+  var form = document.querySelector('form');
+
+  // Adicionando um ouvinte de evento para o envio do formulário
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Evitar o comportamento padrão do formulário (recarregar a página)
+
+      // Obtendo os valores do formulário
+      var nomeEvento = document.getElementById('nomeEvento').value;
+      var dataEvento = document.getElementById('dataEvento').value;
+
+      // Verificando se ambos os campos estão preenchidos
+      if (nomeEvento && dataEvento) {
+          // Criando um objeto de evento
+          var novoEvento = {
+              title: nomeEvento,
+              start: dataEvento, // A data já está no formato correto (yyyy-mm-dd)
+              allDay: true // Evento de dia inteiro
+          };
+
+          // Adicionando o evento ao calendário
+          calendario.addEvent(novoEvento);
+
+          // Limpando os campos do formulário
+          document.getElementById('nomeEvento').value = '';
+          document.getElementById('dataEvento').value = '';
+      } else {
+          alert('Preencha todos os campos do formulário.');
+      }
+  });
+});
